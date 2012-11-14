1256f23b4d0a3a4cc00b77ec8792f994821ad097
David Tolpin <david.tolpin@gmail.com>
2012-11-14 15:54:31 +0200

    better output

--- lab04/scmp/scmp.c
+++ lab04/scmp/scmp.c
@@ -4,6 +4,7 @@
 
 int main(int argc, char **argv) {
 	int cmp;
+	char *rel;
 
 	if(argc!=3) {
 		fprintf(stderr, "usage: %s string1 string2\n", argv[0]);
@@ -13,17 +14,19 @@ int main(int argc, char **argv) {
 	cmp = cmpstr(argv[1], argv[2]);
 	switch(cmp) {
 	case 0:
-		printf("equal\n");
+		rel = "==";
 		break;
 	case 1:
-		printf("greater\n");
+		rel = ">";
 		break;
 	case 2:
-		printf("smaller\n");
+		rel = "<";
 		break;
 	default:
 		assert(0);
 	}
+
+	printf("\"%s\" %s \"%s\"\n", argv[1], rel, argv[2]);
 
 	return cmp;
 }
